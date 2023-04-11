72ab319672d4d5a8a41fd74b9dec877d78b38b00
mibal_ua <9mohapx9@gmail.com>
2023-04-11 14:43:38 +0200

    Init and use Main page

---
+++ src/components/pages/main-page/MainPage.styles.ts
@@ -0,0 +1,10 @@
+import {SxProps} from "@mui/system";
+import {Theme} from "@mui/material";
+
+export const pageWrapper: SxProps<Theme> = {
+    width: '100%',
+    height:'100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+};
---
+++ src/components/pages/main-page/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as styles from './MainPage.styles';
+import {Box, Button} from "@mui/material";
+
+const MainPage = () => {
+    return (
+        <Box sx={styles.pageWrapper}>
+            <Button color='primary'>ги</Button>
+        </Box>
+    );
+};
+
+export default MainPage;
