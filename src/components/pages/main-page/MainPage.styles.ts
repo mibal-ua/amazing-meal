aeb2b0b1017ff3c3965115553bb654816c7097a4
mibal_ua <9mohapx9@gmail.com>
2023-04-19 18:29:13 +0200

    Implement loadingWrapper styles

--- src/components/pages/main-page/MainPage.styles.ts
+++ src/components/pages/main-page/MainPage.styles.ts
@@ -5,3 +5,11 @@ export const pageWrapper: SxProps<Theme> = {
     paddingLeft: '32px',
     paddingRight: '32px',
 };
+
+export const loadingWrapper: SxProps<Theme> = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    minHeight: '100vh',
+};
