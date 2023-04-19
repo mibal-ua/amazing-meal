47c168b1a52ce7d42d992b76c1be1c406feb2c2b
mibal_ua <9mohapx9@gmail.com>
2023-04-19 18:30:17 +0200

    Implement listLayout styles

--- src/components/common/MealCardList/MealCardList.styles.ts
+++ src/components/common/MealCardList/MealCardList.styles.ts
@@ -2,7 +2,8 @@ import {SxProps} from "@mui/system";
 import {Theme} from "@mui/material";
 
 export const listLayout: SxProps<Theme> = {
-    display: 'flex',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(350px, 1fr))',
     gap: '16px',
     paddingTop: '16px',
     paddingBottom: '16px',
