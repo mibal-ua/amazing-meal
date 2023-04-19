7913a0b9a75ae4ad182213ca68c6eec9953f9c7e
mibal_ua <9mohapx9@gmail.com>
2023-04-19 18:27:07 +0200

    Implement `MealService`

---
+++ src/services/meal.service.ts
@@ -0,0 +1,20 @@
+import instance from '@/services/instance';
+import {getAllMealsDTO, getCurrentMealDTO} from '@/types/services';
+
+
+class Meal {
+
+    getAllMeals = async (): Promise<getAllMealsDTO> => {
+        const res = await instance.get('/search.php?f=b');
+        return res.data;
+    };
+
+    getCurrentMeal = async (id: string):Promise<getCurrentMealDTO> => {
+        const res = await instance.get(`/lookup.php?i=${id}`);
+        return res.data;
+    };
+}
+
+const MealService = new Meal();
+
+export default MealService;
