6480e3ffd9c3c58670efeee5bc0e2f5cc753f16e
mibal_ua <9mohapx9@gmail.com>
2023-04-19 17:01:34 +0200

    Fix MainPage styles

--- src/components/pages/main-page/MainPage.styles.ts
+++ src/components/pages/main-page/MainPage.styles.ts
@@ -2,9 +2,6 @@ import {SxProps} from "@mui/system";
 import {Theme} from "@mui/material";
 
 export const pageWrapper: SxProps<Theme> = {
-    width: '100%',
-    height:'100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingLeft: '32px',
+    paddingRight: '32px',
 };
