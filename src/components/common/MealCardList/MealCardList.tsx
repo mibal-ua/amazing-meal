7d1df31a8e0248702a1ccf7241f4f517283fbcac
mibal_ua <9mohapx9@gmail.com>
2023-04-19 18:30:07 +0200

    Change `MealCardListLayout` props

--- src/components/common/MealCardList/MealCardList.tsx
+++ src/components/common/MealCardList/MealCardList.tsx
@@ -1,20 +1,24 @@
 import {Box, Typography} from "@mui/material";
 import React, {FC} from "react";
-import {Meal} from "@/components/pages/main-page/MainPage";
 import MealCard from "@/components/common/mealCard/MealCard";
 import {listLayout} from "@/components/common/MealCardList/MealCardList.styles";
 
 export interface MealCardListLayoutProps {
-    list: Meal[],
+    list: {
+        idMeal: string,
+        strMeal: string,
+        strCategory: string,
+        strMealThumb: string,
+        strTags: string
+    }[] | undefined
 }
-
 
 const MealCardList: FC<MealCardListLayoutProps> = ({list}) => {
     return (<>
         <Typography variant='h2'>Страви</Typography>
         <Box sx={listLayout}>
-            {list.map(({id, name, category, area, image}) =>
-                <MealCard key={id} name={name} category={category} area={area} image={image}
+            {list?.map(({idMeal, strMeal, strCategory, strMealThumb, strTags}) =>
+                <MealCard key={idMeal} name={strMeal} category={strCategory} image={strMealThumb} tags={strTags}
                           onClick={() => alert(name)}/>)}
         </Box>
     </>);
