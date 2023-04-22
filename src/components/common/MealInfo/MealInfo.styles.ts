928419553487341991ec15d5d3f1c682eab5343a
mibal_ua <9mohapx9@gmail.com>
2023-04-22 09:41:00 +0200

    Implement `MealInfo` component

---
+++ src/components/common/MealInfo/MealInfo.styles.ts
@@ -0,0 +1,7 @@
+import {SxProps} from "@mui/system";
+import {Theme} from "@mui/material";
+
+export const layout: SxProps<Theme> = {
+    display: 'flex',
+    gap: '32px'
+};
---
+++ src/components/common/MealInfo/MealInfo.tsx
@@ -0,0 +1,37 @@
+import React, {FC} from "react";
+import {Box, Typography} from "@mui/material";
+import Image from "next/image";
+import {layout} from "@/components/common/MealInfo/MealInfo.styles";
+
+export interface MealInfoProps {
+    name: string,
+    image: string,
+    category: string,
+    tags: string,
+}
+
+const MealInfo: FC<MealInfoProps> = ({name, image, category, tags}) => (
+    <Box sx={layout}>
+        <Box component="img"
+            sx={{
+                height: 280,
+                width: 280,
+                borderRadius: '6px',
+            }}
+            alt='photo'
+            src={image}
+        />
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '8px'
+        }}>
+            <Typography variant='h2'>{name}</Typography>
+            <Typography variant='h4'>Category: {category}</Typography>
+            <Typography variant='h5'>{tags.split(',').join(' | ')}</Typography>
+        </Box>
+
+    </Box>
+);
+
+export default MealInfo;
