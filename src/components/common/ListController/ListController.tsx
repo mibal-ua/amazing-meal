f944bce7ef60bee13d1cf79b95ee202e23a218b3
mibal_ua <9mohapx9@gmail.com>
2023-04-22 09:13:39 +0200

    Implement `ListController` component

--- src/components/common/ListController/ListController.tsx
+++ src/components/common/ListController/ListController.tsx
@@ -1,22 +1,27 @@
 import React, {FC} from "react";
-import {Button} from "@mui/material";
+import {Box, Button} from "@mui/material";
+import {controllerLayout} from "@/components/common/ListController/ListController.styles";
 
 export interface ListControllerProps {
     pageCount: number,
+    current: number,
     // @ts-ignore
-    onChange: fn;
+    onChange: fn,
 }
 
-const ListController: FC<ListControllerProps> = ({pageCount, onChange}) => {
-    const elements = [];
+const ListController: FC<ListControllerProps> = ({pageCount, current, onChange}) => {
+    const buttons = [];
     for (let i = 1; i <= pageCount; i++) {
-        elements.push(<Button onClick={() => onChange(i)}>{i}</Button>);
+        if (i === current) {
+            buttons.push(<Button variant={'contained'} key={i} onClick={() => onChange(i)}>{i}</Button>);
+        } else {
+            buttons.push(<Button key={i} onClick={() => onChange(i)}>{i}</Button>);
+        }
     }
-
     return (
-        <>
-            {elements}
-        </>
+        <Box sx={controllerLayout}>
+            {buttons}
+        </Box>
     );
 };
 
