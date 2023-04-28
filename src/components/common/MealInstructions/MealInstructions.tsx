31b3d5e4cec9643f70b7628108d1d07092625a62
mibal_ua <9mohapx9@gmail.com>
2023-04-28 15:48:38 +0200

    Use arrays instead of obj in `MealInstructions`

--- src/components/common/MealInstructions/MealInstructions.tsx
+++ src/components/common/MealInstructions/MealInstructions.tsx
@@ -5,56 +5,14 @@ import VideoPlayer from '@/components/common/MealVideoGuide/VideoPlayer';
 
 export interface MealInstructionsProps {
     instructions: string,
-    ingredients: {
-        strIngredient1: string,
-        strIngredient2: string,
-        strIngredient3: string,
-        strIngredient4: string,
-        strIngredient5: string,
-        strIngredient6: string,
-        strIngredient7: string,
-        strIngredient8: string,
-        strIngredient9: string,
-        strIngredient10: string,
-        strIngredient11: string,
-        strIngredient12: string,
-        strIngredient13: string,
-        strIngredient14: string,
-        strIngredient15: string,
-        strIngredient16: string,
-        strIngredient17: string,
-        strIngredient18: string,
-        strIngredient19: string,
-        strIngredient20: string,
-    },
-    measures: {
-        strMeasure1: string,
-        strMeasure2: string,
-        strMeasure3: string,
-        strMeasure4: string,
-        strMeasure5: string,
-        strMeasure6: string,
-        strMeasure7: string,
-        strMeasure8: string,
-        strMeasure9: string,
-        strMeasure10: string,
-        strMeasure11: string,
-        strMeasure12: string,
-        strMeasure13: string,
-        strMeasure14: string,
-        strMeasure15: string,
-        strMeasure16: string,
-        strMeasure17: string,
-        strMeasure18: string,
-        strMeasure19: string,
-        strMeasure20: string,
-    },
+    ingredients: string[],
+    measures: string[],
     video: string,
-}
+};
 
 const MealInstructions: FC<MealInstructionsProps> = ({instructions, ingredients, measures, video}) => {
-    const ingredientsValues = Object.values(ingredients).filter((val) => val);
-    const measuresValues = Object.values(measures).filter((val) => val);
+    const ingredientsValues = ingredients.filter((val) => val);
+    const measuresValues = measures.filter((val) => val);
 
     return (
         <Box sx={layout}>
