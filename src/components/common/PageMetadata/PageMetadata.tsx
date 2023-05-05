9ca9a039e24df734b3e77f31e3ef0caf7ccb1a0a
mibal_ua <9mohapx9@gmail.com>
2023-05-05 18:40:59 +0200

    Implement `PageMetadata` component

---
+++ src/components/common/PageMetadata/PageMetadata.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+import Head from "next/head";
+
+interface PageMetadataProps {
+    title: string;
+}
+
+const PageMetadata: FC<PageMetadataProps> = ({
+    title
+}) => (
+    <Head>
+        <title>{title}</title>
+        <link rel="shortcut icon" href="/chicken.png" />
+    </Head>
+);
+
+
+export default PageMetadata;
