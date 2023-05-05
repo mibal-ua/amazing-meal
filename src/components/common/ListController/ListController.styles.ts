1a88863bb19ce27e8bf1e8edf26369e291b31c3c
mibal_ua <9mohapx9@gmail.com>
2023-05-05 19:08:16 +0200

    Use MUI `Pagination` component instead of custom

--- src/components/common/ListController/ListController.styles.ts
+++
@@ -1,8 +0,0 @@
-import { Theme } from "@mui/material";
-import { SxProps } from "@mui/system";
-
-export const controllerLayout: SxProps<Theme> = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-};
--- src/components/common/MealCardList/MealCardList.tsx
+++ src/components/common/MealCardList/MealCardList.tsx
@@ -1,7 +1,6 @@
-import React, { FC, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import React, { ChangeEvent, FC, useState } from "react";
+import { Box, Pagination, Typography } from "@mui/material";
 
-import ListController from "@/components/common/ListController/ListController";
 import MealCard from "@/components/common/MealCard/MealCard";
 import * as styles from "@/components/common/MealCardList/MealCardList.styles";
 import { Meal } from "@/types/services";
@@ -13,50 +12,46 @@ interface MealCardListLayoutProps {
 const PAGE_ELEMENT_COUNT = 10;
 
 const MealCardList: FC<MealCardListLayoutProps> = ({ list }) => {
-    if (!list) {
-        return (
-            <>
-                <Box sx={styles.upperLayout}>
-                    <Typography variant="h2">Страви</Typography>
-                </Box>
-                <Box sx={styles.listLayout}>
-                    <Typography variant="h4">Пустий список(</Typography>
-                </Box>
-            </>
-        );
-    }
+    if (!list) return (<>
+        <Box sx={styles.upperLayout}>
+            <Typography variant="h2">Страви</Typography>
+        </Box>
+        <Box sx={styles.listLayout}>
+            <Typography variant="h4">Пустий список(</Typography>
+        </Box>
+    </>);
 
     const [currentPage, setCurrentPage] = useState(1);
     const pageCount = Math.ceil(list.length / PAGE_ELEMENT_COUNT);
 
     const start = PAGE_ELEMENT_COUNT * (currentPage - 1);
     const end = start + PAGE_ELEMENT_COUNT;
-    return (
-        <>
-            <Box sx={styles.upperLayout}>
-                <Typography variant="h2">Страви</Typography>
-                <ListController
-                    pageCount={pageCount}
-                    current={currentPage}
-                    onChange={(number: number) => setCurrentPage(number)}
-                />
-            </Box>
-            <Box sx={styles.listLayout}>
-                {list
-                    .slice(start, end)
-                    .map(({ idMeal, strMeal, strCategory, strMealThumb, strTags }) => (
-                        <MealCard
-                            key={idMeal}
-                            name={strMeal}
-                            category={strCategory}
-                            image={strMealThumb}
-                            tags={strTags}
-                            link={`/${idMeal}`}
-                        />
-                    ))}
-            </Box>
-        </>
-    );
+    return (<>
+        <Box sx={styles.upperLayout}>
+            <Typography variant="h2">Страви</Typography>
+            <Pagination
+                color="primary"
+                count={pageCount}
+                page={currentPage}
+                onChange={(event: ChangeEvent<unknown>, number: number) =>
+                    setCurrentPage(number)}
+                hidePrevButton hideNextButton />
+        </Box>
+        <Box sx={styles.listLayout}>
+            {list
+                .slice(start, end)
+                .map(({ idMeal, strMeal, strCategory, strMealThumb, strTags }) => (
+                    <MealCard
+                        key={idMeal}
+                        name={strMeal}
+                        category={strCategory}
+                        image={strMealThumb}
+                        tags={strTags}
+                        link={`/${idMeal}`}
+                    />
+                ))}
+        </Box>
+    </>);
 };
 
 export default MealCardList;
