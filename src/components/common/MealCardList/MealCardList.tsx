fe56852424c55fe2c9575aececeb470c63d46e6a
mibal_ua <9mohapx9@gmail.com>
2023-05-05 19:48:51 +0200

    Use `usePageNumber` custom hook

--- src/components/common/MealCardList/MealCardList.tsx
+++ src/components/common/MealCardList/MealCardList.tsx
@@ -1,31 +1,18 @@
 import React, { ChangeEvent, FC, useState } from "react";
 import { Box, Pagination, Typography } from "@mui/material";
 
-import MealCard from "@/components/common/MealCard/MealCard";
 import * as styles from "@/components/common/MealCardList/MealCardList.styles";
+import usePageNumber from "@/hooks/usePageNumber/usePageNumber";
 import { Meal } from "@/types/services";
 
-interface MealCardListLayoutProps {
+interface MealCardListProps {
     list: Meal[] | undefined;
 }
 
-const PAGE_ELEMENT_COUNT = 10;
-
-const MealCardList: FC<MealCardListLayoutProps> = ({ list }) => {
-    if (!list) return (<>
-        <Box sx={styles.upperLayout}>
-            <Typography variant="h2">Страви</Typography>
-        </Box>
-        <Box sx={styles.listLayout}>
-            <Typography variant="h4">Пустий список(</Typography>
-        </Box>
-    </>);
-
+const MealCardList: FC<MealCardListProps> = ({ list }) => {
     const [currentPage, setCurrentPage] = useState(1);
-    const pageCount = Math.ceil(list.length / PAGE_ELEMENT_COUNT);
+    const {data, pageCount} = usePageNumber(list, currentPage);
 
-    const start = PAGE_ELEMENT_COUNT * (currentPage - 1);
-    const end = start + PAGE_ELEMENT_COUNT;
     return (<>
         <Box sx={styles.upperLayout}>
             <Typography variant="h2">Страви</Typography>
@@ -38,18 +25,7 @@ const MealCardList: FC<MealCardListLayoutProps> = ({ list }) => {
                 hidePrevButton hideNextButton />
         </Box>
         <Box sx={styles.listLayout}>
-            {list
-                .slice(start, end)
-                .map(({ idMeal, strMeal, strCategory, strMealThumb, strTags }) => (
-                    <MealCard
-                        key={idMeal}
-                        name={strMeal}
-                        category={strCategory}
-                        image={strMealThumb}
-                        tags={strTags}
-                        link={`/${idMeal}`}
-                    />
-                ))}
+            {data}
         </Box>
     </>);
 };
