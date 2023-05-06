e7a68670f0899b7d03ae6d5e28a5819a79ff36e5
mibal_ua <9mohapx9@gmail.com>
2023-05-06 10:38:20 +0200

    Fix `MealInfo` margins

--- src/components/common/MealInfo/MealInfo.tsx
+++ src/components/common/MealInfo/MealInfo.tsx
@@ -11,16 +11,18 @@ interface MealInfoProps {
 }
 
 const MealInfo: FC<MealInfoProps> = ({ name, image, category, tags }) => (
-    <Box sx={styles.blockLayout}>
-        <Box component="img" sx={styles.imageStyle} alt="photo" src={image} />
-        <Box sx={styles.textLayout}>
-            <Typography variant="h3" sx={styles.h3}>{name}</Typography>
-            <Typography variant="h5" sx={styles.h5}>
-                {tags?.split(",").join(" | ")}
-            </Typography>
-            <Typography variant="h5" sx={styles.h5}>
-                Category: {category}
-            </Typography>
+    <Box>
+        <Box sx={styles.blockLayout}>
+            <Box component="img" sx={styles.imageStyle} alt="photo" src={image} />
+            <Box sx={styles.textLayout}>
+                <Typography variant="h3" sx={styles.h3}>{name}</Typography>
+                <Typography variant="h5" sx={styles.h5}>
+                    {tags?.split(",").join(" | ")}
+                </Typography>
+                <Typography variant="h5" sx={styles.h5}>
+                    Category: {category}
+                </Typography>
+            </Box>
         </Box>
     </Box>
 );
