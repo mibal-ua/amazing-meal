368b01f7c7a415aad994673ba16c78b5dacb764e
mibal_ua <9mohapx9@gmail.com>
2023-05-06 10:36:56 +0200

    Fix `MainPage` and `MealPage` styles

--- src/components/pages/main-page/MainPage.styles.ts
+++ src/components/pages/main-page/MainPage.styles.ts
@@ -2,5 +2,5 @@ import { Theme } from "@mui/material";
 import { SxProps } from "@mui/system";
 
 export const pageWrapper: SxProps<Theme> = {
-    padding: "32px 32px 0 32px",
+    padding: "32px",
 };
--- src/components/pages/meal-page/MealPage.styles.ts
+++ src/components/pages/meal-page/MealPage.styles.ts
@@ -2,16 +2,8 @@ import { Theme } from "@mui/material";
 import { SxProps } from "@mui/system";
 
 export const pageWrapper: SxProps<Theme> = {
-    padding: "16px 128px 32px 128px",
+    padding: "32px",
     display: "flex",
     flexDirection: "column",
-    gap: "20px"
-};
-
-export const loadingWrapper: SxProps<Theme> = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    minHeight: "100vh"
+    gap: "20px",
 };
